b8ea209d6ed0b5fc5c8287e1f804fe7774c07794
Marcus Alagar <mvala079@gmail.com>
2024-03-29 03:21:47 -0500

    Created login loop + menu

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ def main():
     
 def do_main_loop():
   while True:
-    main_choice = ask_main_menu()
+    choice = ask_main_menu()
 
-    if not main_choice.isdigit():
+    if not choice.isdigit():
       print_main_error()
       continue
 
-    match int(main_choice): 
+    match int(choice): 
       case 1:
-        print('Logging in...')
+        do_login_loop()
 
       case 2:
+        # To be implemented later
         print('Signing up...')
 
       case 3:
@@ -26,6 +27,7 @@ def do_main_loop():
         print_main_error()
 
 def ask_main_menu():
+  print()
   print('What would you like to do?')
   print('1. Log in\n' +
         '2. Sign up\n' +
@@ -37,7 +39,41 @@ def ask_main_menu():
 def print_main_error():
   print()
   print('Sorry, that is not a valid input. Please enter 1, 2, or 3.')
+
+def do_login_loop():
+  while True:
+    choice = ask_login_menu()
+
+    if not choice.isdigit():
+      print_login_error()
+      continue
+
+    match int(choice):
+      case 1:
+        print('User')
+
+      case 2: 
+        print('Admin')
+
+      case 3:
+        break
+
+      case _:
+        print_login_error()
+
+def ask_login_menu():
   print()
+  print('Who are you logging in as?')
+  print('1. User\n' +
+        '2. Administrator\n' +
+        '3. Exit\n')
+  
+  choice = input('Enter your choice (1-3) here: ')
+  return choice
+
+def print_login_error():
+  print()
+  print('Sorry, that is not a valid input. Please enter 1, 2, or 3.')
 
 def print_exit_message():
   print()
