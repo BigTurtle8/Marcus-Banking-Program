85a3929fa05d01b4bd11c76a757bf1db109ed0e6
Marcus Alagar <mvala079@gmail.com>
2024-03-29 03:32:54 -0500

    Created admin loop + menu

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def do_login_loop():
         do_user_loop()
 
       case 2: 
-        print('Admin')
+        do_admin_loop()
 
       case 3:
         break
@@ -126,6 +126,43 @@ def ask_user_menu():
 def print_user_error():
   print()
   print('Sorry, that is not a valid input. Please enter 1, 2, 3, 4, 5, or 6.')
+
+def do_admin_loop():
+  while True:
+    choice = ask_admin_menu()
+
+    if not choice.isdigit():
+      print_admin_error()
+      continue
+
+    match int(choice):
+      case 1:
+        # To be implemented later
+        print('Seeing accounts...')
+
+      case 2: 
+        # To be implemented later
+        print('Deleting accounts...')
+
+      case 3:
+        break
+
+      case _:
+        print_admin_error()
+
+def ask_admin_menu():
+  print()
+  print('Who are you logging in as?')
+  print('1. See accounts\n' +
+        '2. Delete accounts\n' +
+        '3. Exit\n')
+  
+  choice = input('Enter your choice (1-3) here: ')
+  return choice
+
+def print_admin_error():
+  print()
+  print('Sorry, that is not a valid input. Please enter 1, 2, or 3.')
 
 def print_exit_message():
   print()
